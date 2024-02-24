d4d368c73f98edc18b4f874c4e6d2270c179d02d
AlimHanif <alim.kidh11@gmail.com>
2024-02-24 21:55:56 +0000

    add push and pull index to redis

---
+++ backend/utils/pull_index.py
@@ -0,0 +1,24 @@
+import json
+import os, sys
+import asyncio
+
+BASEPATH = os.path.dirname(__file__)
+sys.path.append(BASEPATH)
+
+from redis_utils import get_secret_value, get_redis, get_val, get_doc_size
+
+if __name__ == "__main__":
+    # Ask on discord to get the hardcoded config
+    config_redis = get_secret_value(key="redis-test")
+
+    r = get_redis(config_redis)
+
+    word = "w:men"
+
+    index_result = get_val(r, word)
+    document_size = get_doc_size(r)
+
+    doc_ids_list = list(range(document_size))
+
+    # print(index_result)
+    print(doc_ids_list)
