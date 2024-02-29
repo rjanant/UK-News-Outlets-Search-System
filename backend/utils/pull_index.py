fa8a2dc196a727844752f25bf9c6e63f08b2e9a4
AlimHanif <alim.kidh11@gmail.com>
2024-02-29 20:12:37 +0000

    parameterize the db index

--- backend/utils/pull_index.py
+++ backend/utils/pull_index.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import asyncio
 from redis_utils import get_val, get_doc_size, get_doc_ids_list
 
 if __name__ == "__main__":
@@ -7,9 +8,10 @@ if __name__ == "__main__":
     word = "men"
 
     # index_result = get_val(word)
-    document_size = get_doc_size()
-    doc_ids_list = get_doc_ids_list()
+    document_size = asyncio.run(get_doc_size())
+    # document_size = get_doc_size()
+    # doc_ids_list = get_doc_ids_list()
 
     # print(index_result)
     print(document_size)
-    print(len(doc_ids_list))
+    # print(len(doc_ids_list))
