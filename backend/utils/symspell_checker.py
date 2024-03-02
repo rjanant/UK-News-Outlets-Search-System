010335a6c8ef02660bdc8c2bbf967b800daf292b
Patrikas Vanagas <vanagas.p5@gmail.com>
2024-03-02 00:53:50 +0000

    upd

--- backend/utils/symspell_checker.py
+++ backend/utils/symspell_checker.py
@@ -1,4 +1,6 @@
 from typing import List
+from collections import Counter
+import re
 from symspellpy import SymSpell, Verbosity
 import os
 import pandas as pd
@@ -92,6 +94,44 @@ class SpellChecker:
             text, max_edit_distance=max_edit_distance, ignore_non_words=ignore_non_words
         )
         return suggestions[0].term if suggestions else text
+
+
+def norvig_correction(word):
+    "SLOW! Most probable spelling correction for word."
+
+    def words(text):
+        return re.findall(r"\w+", text.lower())
+
+    WORDS = Counter(words(open("spell_checking_files/very_big_string.txt").read()))
+
+    def known(words):
+        "The subset of `words` that appear in the dictionary of WORDS."
+        return set(w for w in words if w in WORDS)
+
+    def P(word, N=sum(WORDS.values())):
+        "Probability of `word`."
+        return WORDS[word] / N
+
+    def candidates(word):
+        "Generate possible spelling corrections for word."
+
+        def edits1(word):
+            "All edits that are one edit away from `word`."
+            letters = "abcdefghijklmnopqrstuvwxyz"
+            splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+            deletes = [L + R[1:] for L, R in splits if R]
+            transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R) > 1]
+            replaces = [L + c + R[1:] for L, R in splits if R for c in letters]
+            inserts = [L + c + R for L, R in splits for c in letters]
+            return set(deletes + transposes + replaces + inserts)
+
+        def edits2(word):
+            "All edits that are two edits away from `word`."
+            return (e2 for e1 in edits1(word) for e2 in edits1(e1))
+
+        return known([word]) or known(edits1(word)) or known(edits2(word)) or [word]
+
+    return max(candidates(word), key=P)
 
 
 # if __name__ == "__main__":
