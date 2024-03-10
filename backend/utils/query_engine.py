8f72fb8f7f7ec0a7e30220205b46872dca248979
Patrikas Vanagas <vanagas.p5@gmail.com>
2024-03-10 02:12:09 +0000

    Add query_expansion.py, update query_engine.py and requirements.txt

--- backend/utils/query_engine.py
+++ backend/utils/query_engine.py
@@ -5,39 +5,53 @@ import time
 import math
 import asyncio
 import sys
+
 sys.path.append(os.path.dirname(__file__))
 from nltk.stem import PorterStemmer
 from typing import DefaultDict, Dict, List, Tuple
 from common import read_file, get_stop_words, get_preprocessed_words
-from redis_utils import get_doc_size, get_doc_ids_list, get_json_values, is_key_exists, get_json_value
+from redis_utils import (
+    get_doc_size,
+    get_doc_ids_list,
+    get_json_values,
+    is_key_exists,
+    get_json_value,
+)
 from build_index import delta_decode_list
 from basetype import RedisKeys
+from query_expander import QueryExpander
 
 # STOP_WORDS_FILE = "ttds_2023_english_stop_words.txt"
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 NUM_OF_CORES = os.cpu_count()
 SPECIAL_PATTERN = {
-    'proximity': re.compile(r"#(\d+)\((\w+),\s*(\w+)\)"),
-    'exact': re.compile(r"\"[^\"]+\""),
-    'spliter': re.compile(r"(AND|OR|NOT|#\d+\(\w+,\s*\w+\)|\"[^\"]+\"|\'[^\']+\'|\w+|\(|\))")
+    "proximity": re.compile(r"#(\d+)\((\w+),\s*(\w+)\)"),
+    "exact": re.compile(r"\"[^\"]+\""),
+    "spliter": re.compile(
+        r"(AND|OR|NOT|#\d+\(\w+,\s*\w+\)|\"[^\"]+\"|\'[^\']+\'|\w+|\(|\))"
+    ),
 }
 
-def preprocess_match(match: re.Match, stopping: bool = True, stemming: bool = True) -> str:
+
+def preprocess_match(
+    match: re.Match, stopping: bool = True, stemming: bool = True
+) -> str:
     word = match.group(0)
     if word in ["AND", "OR", "NOT"]:
         return word
     word = word.lower()
-    
+
     stopwords = get_stop_words()
     if stopping and word in stopwords:
         return ""
-    
+
     if stemming:
         stemmer = PorterStemmer()
         word = stemmer.stem(word)
 
     return word
+
 
 def load_queries(file_name: str) -> list:
     query_lines = read_file(file_name).split("\n")
@@ -48,6 +62,7 @@ def load_queries(file_name: str) -> list:
         query_id, query_text = line.split(":")
         queries.append(query_text.strip())
     return queries
+
 
 def handle_binary_operator(operator: str, left: list, right: list) -> list:
     # print("handle binary operator", operator, left, right)
@@ -60,19 +75,22 @@ def handle_binary_operator(operator: str, left: list, right: list) -> list:
         print("OR operation")
         return list(set(left) | set(right))
 
+
 def handle_not_operator(operand: List[int], doc_ids_list: List[int]) -> List[int]:
     print("NOT operation")
     if operand is None:
         return doc_ids_list
     return list(set(doc_ids_list) - set(operand))
 
+
 async def get_doc_ids_from_string(string: str) -> List[int]:
-    # check if string is a phrase bounded by double quotes 
+    # check if string is a phrase bounded by double quotes
     if await is_key_exists(RedisKeys.index(string)):
         term_index = await get_json_value(RedisKeys.index(string))
         return list(map(int, term_index.keys()))
     else:
         return []
+
 
 async def get_doc_ids_from_pattern(pattern: str) -> List[int]:
     # pattern is of the form "A B"/"A B C" etc
@@ -85,7 +103,15 @@ async def get_doc_ids_from_pattern(pattern: str) -> List[int]:
         positions = delta_decode_list(words_index[0][doc_id])
         for pos in positions:
             try:
-                if all([pos + i in words_index[i][doc_id] for i in range(1, len(words))]) and doc_id not in doc_ids:
+                if (
+                    all(
+                        [
+                            pos + i in words_index[i][doc_id]
+                            for i in range(1, len(words))
+                        ]
+                    )
+                    and doc_id not in doc_ids
+                ):
                     doc_ids.append(int(doc_id))
             except:
                 pass
@@ -96,17 +122,25 @@ async def get_doc_ids_from_pattern(pattern: str) -> List[int]:
 def negate_doc_ids(doc_ids: List[int], doc_ids_list: List[int]) -> List[int]:
     return list(set(doc_ids_list) - set(doc_ids))
 
+
 async def evaluate_proximity_pattern(n: int, w1: str, w2: str) -> List[int]:
     # find all the doc_ids for w1 and w2
     doc_ids_for_w1 = await get_doc_ids_from_string(w1)
     # find the doc_ids that satisfy the condition
     doc_ids = []
     values = await get_json_values([RedisKeys.index(w1), RedisKeys.index(w2)])
+
     async def process_doc_id(doc_id):
         try:
             positions_for_w1 = delta_decode_list(values[0][doc_id])
             positions_for_w2 = delta_decode_list(values[1][doc_id])
-            if any([abs(pos1 - pos2) <= int(n) for pos1 in positions_for_w1 for pos2 in positions_for_w2]):
+            if any(
+                [
+                    abs(pos1 - pos2) <= int(n)
+                    for pos1 in positions_for_w1
+                    for pos2 in positions_for_w2
+                ]
+            ):
                 return int(doc_id)
         except:
             pass
@@ -116,10 +150,13 @@ async def evaluate_proximity_pattern(n: int, w1: str, w2: str) -> List[int]:
     doc_ids = [result for result in results if result is not None]
     return doc_ids
 
-async def evaluate_subquery(subquery: str, special_patterns: Dict[str, re.Pattern]) -> List[int]:
-    
-    proximity_match = re.match(special_patterns['proximity'], subquery)
-    exact_match = re.match(special_patterns['exact'], subquery)
+
+async def evaluate_subquery(
+    subquery: str, special_patterns: Dict[str, re.Pattern]
+) -> List[int]:
+
+    proximity_match = re.match(special_patterns["proximity"], subquery)
+    exact_match = re.match(special_patterns["exact"], subquery)
     if proximity_match:
         n = proximity_match.group(1)
         w1 = proximity_match.group(2)
@@ -134,12 +171,14 @@ async def evaluate_subquery(subquery: str, special_patterns: Dict[str, re.Patter
             print("handle word(s)", subquery)
             return await get_doc_ids_from_string(subquery)
 
-async def calculate_tf_idf(tokens: List[str],
-                           doc_id: str,
-                           docs_size: int,
-                           word_freq: Dict[str, int],
-                           doc_freq: Dict[str, int]
-                        ) -> float:
+
+async def calculate_tf_idf(
+    tokens: List[str],
+    doc_id: str,
+    docs_size: int,
+    word_freq: Dict[str, int],
+    doc_freq: Dict[str, int],
+) -> float:
     tf_idf_score = 0
     for token in tokens:
         keyword = f"{token}:{doc_id}"
@@ -147,11 +186,11 @@ async def calculate_tf_idf(tokens: List[str],
         #     continue
         if not await is_key_exists(RedisKeys.index(token)):
             continue
-        
+
         # token_index = await get_json_value(RedisKeys.index(token)) # Deprecated
         if keyword not in word_freq:
             continue
-        
+
         tf = 1 + math.log10(word_freq[keyword])
         idf = math.log10(docs_size / doc_freq[token])
         tf_idf_score += tf * idf
@@ -169,14 +208,17 @@ def precedence(operator: str) -> int:
     else:
         return -1
 
+
 def associativity(operator: str) -> str:
     if operator == "NOT":
         return "right"
     else:
         return "left"
 
+
 def is_operator(token: str) -> bool:
     return token in ["AND", "OR", "NOT"]
+
 
 def infix_to_postfix(query: str, spliter: re.Pattern) -> list:
     tokens = re.findall(spliter, query)
@@ -184,17 +226,28 @@ def infix_to_postfix(query: str, spliter: re.Pattern) -> list:
     postfix = []
     for token in tokens:
         if is_operator(token):
-            while stack and is_operator(stack[-1]) and \
-                ((associativity(token) == "left" and precedence(token) <= precedence(stack[-1])) or \
-                (associativity(token) == "right" and precedence(token) < precedence(stack[-1]))):
+            while (
+                stack
+                and is_operator(stack[-1])
+                and (
+                    (
+                        associativity(token) == "left"
+                        and precedence(token) <= precedence(stack[-1])
+                    )
+                    or (
+                        associativity(token) == "right"
+                        and precedence(token) < precedence(stack[-1])
+                    )
+                )
+            ):
                 postfix.append(stack.pop())
             stack.append(token)
-        elif token == '(':
+        elif token == "(":
             stack.append(token)
-        elif token == ')':
-            while stack and stack[-1] != '(':
+        elif token == ")":
+            while stack and stack[-1] != "(":
                 postfix.append(stack.pop())
-            if stack and stack[-1] == '(':
+            if stack and stack[-1] == "(":
                 stack.pop()
         else:
             postfix.append(token)
@@ -202,31 +255,34 @@ def infix_to_postfix(query: str, spliter: re.Pattern) -> list:
         postfix.append(stack.pop())
     return postfix
 
+
 def is_valid_query(query: str) -> bool:
     # check if the query is valid
-    spliter = re.compile(r"(AND|OR|NOT|#\d+\(\w+,\s*\w+\)|\"[^\"]+\"|\'[^\']+\'|\w+|\(|\))")
+    spliter = re.compile(
+        r"(AND|OR|NOT|#\d+\(\w+,\s*\w+\)|\"[^\"]+\"|\'[^\']+\'|\w+|\(|\))"
+    )
     tokens = re.findall(spliter, query)
     prev_token = None
     parentheses_count = 0
     for token in tokens:
-        if token == '(':
+        if token == "(":
             parentheses_count += 1
-        elif token == ')':
+        elif token == ")":
             parentheses_count -= 1
             if parentheses_count < 0:
                 print("Parentheses count is less than 0")
                 return False
         elif token == "NOT":
-            if prev_token and (not is_operator(prev_token) and prev_token != '('):
+            if prev_token and (not is_operator(prev_token) and prev_token != "("):
                 print("Invalid NOT position")
                 return False
         elif is_operator(token):
-            if prev_token and (prev_token == '(' or is_operator(prev_token)):
+            if prev_token and (prev_token == "(" or is_operator(prev_token)):
                 print("Invalid operator position")
                 return False
         else:
             # token is an operand
-            if prev_token and prev_token == ')':
+            if prev_token and prev_token == ")":
                 print("Invalid operand position")
                 return False
         prev_token = token
@@ -234,24 +290,31 @@ def is_valid_query(query: str) -> bool:
         return False
     return True
 
-async def evaluate_boolean_query(query: str,
-                                 doc_ids_list: List[int],
-                                 stopping: bool = True,
-                                 stemming: bool = True,
-                                 special_patterns: dict[str, re.Pattern] = SPECIAL_PATTERN) -> List:
+
+async def evaluate_boolean_query(
+    query: str,
+    doc_ids_list: List[int],
+    stopping: bool = True,
+    stemming: bool = True,
+    special_patterns: dict[str, re.Pattern] = SPECIAL_PATTERN,
+) -> List:
     # query = " ".join([token.lower() if token not in ["AND", "OR", "NOT"] else token for token in query.split("\w+ ")])
     query = re.sub(r"(\w+)", lambda x: preprocess_match(x, stopping, stemming), query)
     print(query)
     if not is_valid_query(query):
         print("Invalid query: ", query)
         return []
-    
-    postfix = infix_to_postfix(query, special_patterns['spliter'])
-    
+
+    postfix = infix_to_postfix(query, special_patterns["spliter"])
+
     print("postfix", postfix)
 
     # evalute the value for the stuff first
-    tasks = [evaluate_subquery(token, special_patterns) for token in postfix if not is_operator(token)]
+    tasks = [
+        evaluate_subquery(token, special_patterns)
+        for token in postfix
+        if not is_operator(token)
+    ]
     results = await asyncio.gather(*tasks)
     for idx, token in enumerate(postfix):
         if not is_operator(token):
@@ -277,23 +340,44 @@ async def evaluate_boolean_query(query: str,
         # print the processing error term
         traceback.print_exc()
         exit()
-        
-async def evaluate_ranked_query(query: str, docs_size:int, stopping: bool = True, stemming:bool = True) -> List[Tuple[int, float]]:
+
+
+query_expander = QueryExpander()
+query_expander = QueryExpander(model_path="word2vec_files/word2vec_200_10.model")
+
+
+async def evaluate_ranked_query(
+    query: str,
+    docs_size: int,
+    stopping: bool = True,
+    stemming: bool = True,
+    expand_query: bool = True,
+    n_expand: int = 3,
+) -> List[Tuple[int, float]]:
+    if expand_query:
+        query = query_expander.expand_query(query, top_n=n_expand)
     words = get_preprocessed_words(query, stopping, stemming)
     doc_ids = set()
     word_freq = dict()
     doc_freq = dict()
-    doc_ids_tasks = [get_json_value(RedisKeys.index(word)) for word in words if await is_key_exists(RedisKeys.index(word))]
+    doc_ids_tasks = [
+        get_json_value(RedisKeys.index(word))
+        for word in words
+        if await is_key_exists(RedisKeys.index(word))
+    ]
     doc_ids_results = await asyncio.gather(*doc_ids_tasks)
     for word, result in zip(words, doc_ids_results):
         doc_ids = doc_ids.union(result.keys())
         word_freq.update({f"{word}:{doc}": len(pos) for doc, pos in result.items()})
         doc_freq[word] = len(result)
-    
+
     doc_ids = list(doc_ids)
     scores = []
-    
-    score_tasks = [calculate_tf_idf(words, doc_id, docs_size, word_freq, doc_freq) for doc_id in doc_ids]
+
+    score_tasks = [
+        calculate_tf_idf(words, doc_id, docs_size, word_freq, doc_freq)
+        for doc_id in doc_ids
+    ]
     score_results = await asyncio.gather(*score_tasks)
     for idx, doc_id in enumerate(doc_ids):
         scores.append((doc_id, score_results[idx]))
@@ -302,28 +386,41 @@ async def evaluate_ranked_query(query: str, docs_size:int, stopping: bool = True
 
     return scores
 
-async def boolean_test(boolean_queries: List[str] = ["\"Comic Relief\" AND (NOT wtf OR #1(Comic, Relief))"]) -> List[List[int]]:
+
+async def boolean_test(
+    boolean_queries: List[str] = ['"Comic Relief" AND (NOT wtf OR #1(Comic, Relief))'],
+) -> List[List[int]]:
     doc_ids_list = await get_doc_ids_list()
     start_time = time.time()
     results = []
     for query in boolean_queries:
         results.append(await evaluate_boolean_query(query, doc_ids_list))
-    print("Time taken to process boolean queries", time.time() - start_time, len(results))
+    print(
+        "Time taken to process boolean queries", time.time() - start_time, len(results)
+    )
     return results
 
-async def ranked_test(ranked_queries: List[str] = ["Comic Relief"]) -> List[List[Tuple[int, float]]]:
+
+async def ranked_test(
+    ranked_queries: List[str] = ["Comic Relief"],
+) -> List[List[Tuple[int, float]]]:
     doc_size = await get_doc_size()
     start_time = time.time()
     results = []
     for query in ranked_queries:
         results.append(await evaluate_ranked_query(query, doc_size))
-    print("Time taken to process ranked queries", time.time() - start_time, len(results[0]))
+    print(
+        "Time taken to process ranked queries",
+        time.time() - start_time,
+        len(results[0]),
+    )
     return results
-    
+
+
 async def main():
     print(await boolean_test())
     # await ranked_test()
-    
+
     #### BENCHMARKING
     # result = await ranked_test()
     # print(result[0][:5])
@@ -336,18 +433,18 @@ async def main():
     # Baseline: Donald Trump and Biden in 2024 USA: 3.79338002204895 1765
     # Improvement: Donald Trump and Biden in 2024 USA: 0.5762429237365723 1765
 
-    # Inference Result Query 1    
+    # Inference Result Query 1
     # Baseline: Comic Relief: [('219148', 6.715275777697791), ('220261', 6.715275777697791), ('222097', 6.715275777697791), ('312028', 6.715275777697791), ('313037', 6.5266929715344775)]
     # Improvement: Comic Relief: [('219148', 6.715275777697791), ('220261', 6.715275777697791), ('222097', 6.715275777697791), ('312028', 6.715275777697791), ('313037', 6.5266929715344775)]
-    
-    # Inference Result Query 2    
+
+    # Inference Result Query 2
     # Baseline: Donald Trump and Biden in 2024 USA: [('224817', 21.52595725971649), ('220825', 21.123974002575643), ('222614', 20.60367423116946), ('221260', 19.128669330853047), ('222408', 18.69979137178141)]
     # Improvement: Donald Trump and Biden in 2024 USA: [('224817', 21.52595725971649), ('220825', 21.123974002575643), ('222614', 20.60367423116946), ('221260', 19.128669330853047), ('222408', 18.69979137178141)]
 
 
 if __name__ == "__main__":
     asyncio.run(main())
-    
+
     # # ### processing ranked queries
     # ranked_queries = read_ranked_queries("queries.ranked.txt")
     # start = time.time()
